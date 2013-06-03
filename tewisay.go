909fbcf4e286aafbf11f54453cde9fa318387405
lucy <ne.tetewi@gmail.com>
2013-06-03 22:27:37 +0200

    implement tewithink, clean up code

--- tewisay.go
+++ tewisay.go
@@ -5,41 +5,40 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
 	"strings"
 	"unicode/utf8"
 )
 
-var cowfile = flag.String("f", "", "what cowfile to use")
+var (
+	cowfile = flag.String("f", "", "what cowfile to use")
+	think   = path.Base(os.Args[0]) == "tewithink"
+)
 
 const (
 	upper = "_"
+	lower = "─"
+
+	line  = "\\"
 	left  = "|"
 	right = "|"
-	lower = "─"
+
+	tline  = "o"
+	tleft  = "("
+	tright = ")"
 )
 
-var toReplace = map[string]string{
-	"$the_cow = <<EOC;\n": "",
-	"\nEOC":               "",
-	"$thoughts":           "\\",
-}
-
-func balloon(text string) string {
-	l := utf8.RuneCountInString(text)
-	return fmt.Sprintf(""+
-		" %s\n"+
-		"%s %s %s\n"+
-		" %s",
-		strings.Repeat(upper, l+2),
-		left, text, right,
-		strings.Repeat(lower, l+2))
-}
-
 func prepare(cow string) string {
-	for old, neu := range toReplace {
+	for old, neu := range map[string]string{
+		"$the_cow = <<EOC;\n": "",
+		"\nEOC":               "",
+	} {
 		cow = strings.Replace(cow, old, neu, -1)
 	}
-	return cow
+	if think {
+		return strings.Replace(cow, "$thoughts", tline, -1)
+	}
+	return strings.Replace(cow, "$thoughts", line, -1)
 }
 
 func getCowfile(name string) (string, error) {
@@ -50,13 +49,30 @@ func getCowfile(name string) (string, error) {
 		}
 		name = cowpath + "/" + name + ".cow"
 	}
+
 	file, err := os.Open(name)
+	defer file.Close()
 	if err != nil {
 		return "", err
 	}
-	defer file.Close()
 	out, err := ioutil.ReadAll(file)
 	return string(out), err
+}
+
+func balloon(text string) string {
+	var (
+		l    = utf8.RuneCountInString(text)
+		up   = strings.Repeat(upper, l+2)
+		down = strings.Repeat(lower, l+2)
+	)
+
+	if think {
+		return fmt.Sprintf(" %s\n%s %s %s\n %s",
+			up, tleft, text, tright, down)
+	} else {
+		return fmt.Sprintf(" %s\n%s %s %s\n %s",
+			up, left, text, right, down)
+	}
 }
 
 func main() {
